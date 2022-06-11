19ca9519c66deb5101ead92fe3d1226830850163
priyanka323 <priyarankawat88160@gmail.com>
2022-06-11 16:59:16 +0530

    Navbar,landing,section,signup are ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './component/Navbar/Navbar';
+import Landing from './component/Landing/Landing';
+import Section from './component/Section/Section';
+import Page from './component/Page/Page';
+import Signup from './component/Signup/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Landing/>
+      <Section/>
+      <Page/>
+      {/* <Signup/> */}
+      {/* <div>
+        <Router>
+            <Routes>
+              
+              <Route path="/Signup" element={<Signup />}></Route>
+            
+            </Routes>
+        </Router>
+     </div> */}
+    </>
   );
 }
 
